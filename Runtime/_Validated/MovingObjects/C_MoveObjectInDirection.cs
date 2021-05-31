4b9a679020661fff818d160ae782b5cbf00f4ee0
PEGASUS\craig <thegaminggumby@gmail.com>
2021-05-31 16:59:30 +1000

    Adding move platform update

--- Runtime/_Validated/MovingObjects/C_MoveObjectInDirection.cs
+++ Runtime/_Validated/MovingObjects/C_MoveObjectInDirection.cs
@@ -9,6 +9,7 @@ public class C_MoveObjectInDirection : MonoBehaviour
     public float MovementDistance = 1.0f;
     public float MovementSpeed = 1.0f;
     public bool useEndLocation = false;
+    private Vector3 startLocation;
     public Vector3 endLocation;
     //[SerializeField]
     bool isReversing = false;
@@ -23,7 +24,9 @@ public class C_MoveObjectInDirection : MonoBehaviour
     //[SerializeField]
     private float journeyLength;
     //[SerializeField]
-    private Vector3 startLocation;
+    
+    [SerializeField]bool isrunning = true;
+    bool PlayerAttached = false;
 
     void Start()
     {
@@ -35,43 +38,77 @@ public class C_MoveObjectInDirection : MonoBehaviour
         journeyLength = Vector3.Distance(startLocation, endLocation);
     }
 
+    void movePlatform() 
+    {
+        // Distance moved equals elapsed time times speed..
+        distCovered = (Time.time - startTime) * MovementSpeed;
+        //distCovered = (transform.position - endLocation).magnitude/journeyLength;
+        // Fraction of journey completed equals current distance divided by total distance.
+        fractionOfJourney = distCovered / journeyLength;
+       // fractionOfJourney = distCovered;
+        //fractionOfJourney = Mathf.Clamp(Mathf.Sin(Time.time), 0, 1.0f); //Mathf.Sin(Time.time);
+
+        if (isReversing)
+        {
+            transform.position = Vector3.Lerp(endLocation, startLocation, fractionOfJourney);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(startLocation, endLocation, fractionOfJourney);
+        }
+
+        if (fractionOfJourney > 1.0)
+        {
+            //Alternate based upon move type behavour here
+            isReversing = !isReversing;
+            startTime = Time.time;
+            distCovered = 0;
+            fractionOfJourney = 0;
+            HandlemovementType();
+            //DisableMovement if only once
+
+        }
+        else if (fractionOfJourney < 0)
+        {
+            //Alternate based upon move type behavour here
+            isReversing = !isReversing;
+            startTime = Time.time;
+            distCovered = 0;
+            fractionOfJourney = 0;
+            HandlemovementType();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.activeSelf)
+        if (isrunning)
         {
-            
-            // Distance moved equals elapsed time times speed..
-            distCovered = (Time.time - startTime) * MovementSpeed;
+            movePlatform();            
+        }
+    }
 
-            // Fraction of journey completed equals current distance divided by total distance.
-            fractionOfJourney = distCovered / journeyLength;
-            //fractionOfJourney = Mathf.Clamp(Mathf.Sin(Time.time), 0, 1.0f); //Mathf.Sin(Time.time);
-            if (isReversing)
-            {
-                transform.position = Vector3.Lerp(endLocation, startLocation, fractionOfJourney);
-            }
-            else 
-            {
-                transform.position = Vector3.Lerp(startLocation, endLocation, fractionOfJourney);
-            }           
-            if (fractionOfJourney >= 1.0)
-            {
-                isReversing = !isReversing;
-                startTime = Time.time;
-                distCovered = 0;
-                fractionOfJourney = 0;
-
-                //DisableMovement if only once
-
-            }
-            else if (fractionOfJourney <= 0)
-            {
-                isReversing = !isReversing;
-                startTime = Time.time;
-                distCovered = 0;
-                fractionOfJourney = 0;
-            }
+    void HandlemovementType()
+    {
+        switch (MovementType)
+        {
+            case MoveTypes.LoopOnce:
+                if (isReversing)
+                {
+                    isrunning = true;
+                }
+                else 
+                {
+                    isrunning = false;
+                }
+                
+                break;
+            case MoveTypes.MoveOnce:
+                isrunning = false;
+                break;
+            case MoveTypes.PingPong:
+                isrunning = true;
+                break;
         }
     }
     void CalculateEndLocation()
@@ -100,5 +137,23 @@ public class C_MoveObjectInDirection : MonoBehaviour
                     break;
             }
         }        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerAttached = true;
+            print("Player is Attached");
+            other.gameObject.transform.parent = transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerAttached = false;
+            print("Player is Attached");
+            other.gameObject.transform.parent = null;
+        }
     }
 }
